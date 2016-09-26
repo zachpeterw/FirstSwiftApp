10ae2642dfd77b9f38ec1ef6b9fa9e667b632cbd
zachpeterw <zwhi8516@740s-pl-129.local>
2016-09-26 13:43:30 -0600

    added random color generator, implemented in button

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController
     @IBOutlet private weak var firstButton: UIButton!
     @IBOutlet private weak var firstSwitch: UISwitch!
     
+    @IBOutlet private var appBackground: UIView!
 
     
     override func viewDidLoad() {
@@ -28,8 +29,28 @@ class ViewController: UIViewController
     @IBAction func firstButtonClick(sender: UIButton)
     {
         textLabel.text = "You clicked the button!"
+        textLabel.textColor = makeRandomColor()
+        appBackground.backgroundColor = makeRandomColor()
     }
-
+    @IBAction func firstSwitchclick(sender: UISwitch)
+    {
+        
+    }
+    private func makeRandomColor() -> UIColor
+    {
+        let randomColor : UIColor
+        //color in swift is a percentage - from 0-1 inclusive.
+        //of type CGFloat for all components; RGBA
+        let redColor :CGFloat = CGFloat(drand48())
+        let greenColor :CGFloat = CGFloat(Double(arc4random_uniform(256))/255.00)
+        let blueColor :CGFloat = CGFloat(drand48())
+        let alphaChannel :CGFloat = 1.0
+        
+        randomColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: alphaChannel)
+        
+        
+        return randomColor
+    }
 
 }
 
