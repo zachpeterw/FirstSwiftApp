f4b0c15c559580f5b9d1d33d627e7124675d7058
zachpeterw <zwhi8516@740s-pl-129.local>
2016-09-26 12:37:59 -0600

    added GUI components and the @IBOutlets and @IBActions

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var firstButton: UIButton!
+    @IBOutlet private weak var firstSwitch: UISwitch!
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +24,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func firstButtonClick(sender: UIButton)
+    {
+        textLabel.text = "You clicked the button!"
     }
 
 
